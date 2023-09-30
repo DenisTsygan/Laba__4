2143b078cb2fd7d738775f596cb556e84b7c0018
DenisTsygan <denis.www.denis7@gmail.com>
2023-09-30 10:18:32 +0300

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4
+{
+    public class Matrix
+    {
+        private int rows;
+        private int cols;
+        private int[,] elementsMatrix;
+
+        private Matrix(int rows, int cols)
+        {
+            if (rows > 0 && cols > 0)
+            {
+                this.rows = rows;
+                this.cols = cols;
+                this.elementsMatrix = new int[rows, cols];
+            }
+            else
+            {
+                this.rows = 0;
+                this.cols = 0;
+                this.elementsMatrix = new int[0, 0];
+            }
+        }
+        public Matrix(int[,] elements)
+        {
+            this.rows = elements.GetLength(0);
+            this.cols = elements.GetLength(1);
+            this.elementsMatrix = elements;
+        }
+        ~Matrix()
+        {
+            Console.WriteLine("Matrix was deleted");
+        }
+
+        public bool isSimetricMatrix()
+        {
+            if (rows != cols)
+            {
+                return false;
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (this.elementsMatrix[i, j] != this.elementsMatrix[j, i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+
+        public static Matrix operator +(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if (firstMatrix is null || secondMatrix is null || firstMatrix.rows != secondMatrix.rows || firstMatrix.cols != secondMatrix.cols)
+            {
+                return null;
+            }
+
+            Matrix resultMatrix = new Matrix(firstMatrix.rows, firstMatrix.cols);
+
+            for (int i = 0; i < firstMatrix.rows; i++)
+            {
+                for (int j = 0; j < firstMatrix.cols; j++)
+                {
+                    resultMatrix.elementsMatrix[i, j] = firstMatrix.elementsMatrix[i, j] + secondMatrix.elementsMatrix[i, j];
+                }
+            }
+
+            return resultMatrix;
+        }
+        public static Matrix operator -(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if (firstMatrix is null || secondMatrix is null || firstMatrix.rows != secondMatrix.rows || firstMatrix.cols != secondMatrix.cols)
+            {
+                return null;
+            }
+
+            Matrix resultMatrix = new Matrix(firstMatrix.rows, firstMatrix.cols);
+
+            for (int i = 0; i < firstMatrix.rows; i++)
+            {
+                for (int j = 0; j < firstMatrix.cols; j++)
+                {
+                    resultMatrix.elementsMatrix[i, j] = firstMatrix.elementsMatrix[i, j] - secondMatrix.elementsMatrix[i, j];
+                }
+            }
+
+            return resultMatrix;
+        }
+        public static Matrix operator *(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if (firstMatrix is null || secondMatrix is null || firstMatrix.cols != secondMatrix.rows)
+            {
+                return null;
+            }
+            Matrix resultMatrix = new Matrix(firstMatrix.rows, secondMatrix.cols);
+
+            for (int i = 0; i < firstMatrix.rows; i++)
+            {
+                for (int j = 0; j < secondMatrix.cols; j++)
+                {
+                    resultMatrix.elementsMatrix[i, j] = 0;
+                    for (int k = 0; k < firstMatrix.cols; k++)
+                    {
+                        resultMatrix.elementsMatrix[i, j] += firstMatrix.elementsMatrix[i, k] * secondMatrix.elementsMatrix[k, j];
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static Matrix operator *(int value, Matrix matrix)
+        {
+            Matrix result = new Matrix(matrix.rows, matrix.cols);
+            for (int i = 0; i < matrix.rows; i++)
+            {
+                for (int j = 0; j < matrix.cols; j++)
+                {
+                    result.elementsMatrix[i, j] = matrix.elementsMatrix[i, j] * value;
+                }
+            }
+
+            return result;
+        }
+        public override string ToString()
+        {
+            string res = "";
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    res += $"{this.elementsMatrix[i, j]}  \t";
+                }
+                res += "\n";
+            }
+
+            return res;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Laba_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] elementsMatrixA = { { 2, 3, 4 }, { 1, -2, 0 }, { 0, 1, 2 } };
+            Matrix matrixA = new(elementsMatrixA);
+            int[,] elementsMatrixB = { { 2, 0, -2 }, { 1, 1, 0 }, { 1, -1, 1 } };
+            Matrix matrixB = new(elementsMatrixB);
+            Console.WriteLine("Matrix A:");
+            Console.WriteLine(matrixA);
+            Console.WriteLine("Matrix B:");
+            Console.WriteLine(matrixB);
+            Matrix matrixX = 2 * matrixA * (matrixA + matrixB) - 3 * matrixA;
+
+            Console.WriteLine("X=2A*(A+B)-3A");
+            if (matrixX is not null)
+            {
+                Console.WriteLine("Matrix X:");
+                Console.WriteLine(matrixX);
+
+                Console.WriteLine($"Matrix X simetric ={matrixX.isSimetricMatrix()}");
+            }
+            else
+            {
+                Console.WriteLine("Matrix X was not created");
+            }
+            /*
+            Console.WriteLine("Test on simetric another matrix:");
+            Console.WriteLine("");
+            int[,] elementsMatrixS = { { 1, 0, -3 }, { 0, 1, 0 }, { -3, 0, 1 } };
+            Matrix matrixS = new(elementsMatrixS);
+            Console.WriteLine("Matrix S:");
+            Console.WriteLine(matrixS);
+            Console.WriteLine($"Matrix S simetric ={matrixS.isSimetricMatrix()}");
+            */
+        }
+    }
+}
